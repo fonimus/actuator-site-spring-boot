500cc1a9ec1593dc4d7220a305e40a6364a300c4
Francois Onimus <francois.onimus@gmail.com>
2018-10-21 23:25:36 +0200

    Fix project without actuator dependency failure

--- starter/src/main/kotlin/com/github/fonimus/actuator/site/ActuatorSiteAutoConfiguration.kt
+++ starter/src/main/kotlin/com/github/fonimus/actuator/site/ActuatorSiteAutoConfiguration.kt
@@ -1,7 +1,7 @@
 package com.github.fonimus.actuator.site
 
 import org.slf4j.LoggerFactory
-import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
@@ -14,9 +14,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
  *
  * Activated by default. Disable it with actuator.site.enabled=false
  */
+@ConditionalOnClass(name = ["org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties"])
 @ConditionalOnProperty(name = ["actuator.site.enabled"], havingValue = "true", matchIfMissing = true)
 @ConfigurationProperties(prefix = "actuator.site")
-class ActuatorSiteAutoConfiguration(private val properties: WebEndpointProperties) : WebMvcConfigurer {
+class ActuatorSiteAutoConfiguration(private val properties: org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties) : WebMvcConfigurer {
 
     val log = LoggerFactory.getLogger(ActuatorSiteAutoConfiguration::class.java)
 
